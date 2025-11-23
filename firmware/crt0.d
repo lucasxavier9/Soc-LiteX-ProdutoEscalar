013bda3136f86fc3ab2a712f9da80f2e5c85b358
lucasxavier9 <z7wedryson@gmail.com>
2025-11-23 00:25:27 -0300

    organizando

--- firmware/crt0.d
+++
@@ -1,4 +0,0 @@
-crt0.o: \
- /home/z7/Desktop/eda/oss-cad-suite/litex/litex/litex/soc/cores/cpu/picorv32/crt0.S \
- /home/z7/Desktop/eda/oss-cad-suite/litex/litex/litex/soc/cores/cpu/picorv32/extraops.S
-/home/z7/Desktop/eda/oss-cad-suite/litex/litex/litex/soc/cores/cpu/picorv32/extraops.S:
